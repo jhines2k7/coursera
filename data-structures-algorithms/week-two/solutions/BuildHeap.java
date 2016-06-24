c83c8b4c0cd5a5cf5aa4785e3681f67288639b49
James Hines <jhines2k7@yahoo.com>
2016-06-24 17:19:29 -0400

    Create BuildHeap.java

---
+++ data-structures-algorithms/week-two/solutions/BuildHeap.java
@@ -0,0 +1,117 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BuildHeap {
+    private int[] data;
+    private List<Swap> swaps;
+
+    private FastScanner in;
+    private PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        new BuildHeap().solve();
+    }
+
+    private void readData() throws IOException {
+        int n = in.nextInt();
+        data = new int[n];
+        for (int i = 0; i < n; ++i) {
+            data[i] = in.nextInt();
+        }
+    }
+
+    private void writeResponse() {
+        out.println(swaps.size());
+        for (Swap swap : swaps) {
+            out.println(swap.index1 + " " + swap.index2);
+        }
+    }
+
+    private void generateSwaps() {
+        swaps = new ArrayList<Swap>();
+        buildHeap();
+    }
+
+    private int leftChild(int i) {
+        return (2 * i) + 1;
+    }
+
+    private int rightChild(int i) {
+        return (2 * i) + 2;
+    }
+
+    private void buildHeap() {
+        for (int i = (int) Math.floor(data.length / 2); i >= 0; i--) {
+            siftDown(i);
+        }
+    }
+
+    private void siftDown(int i) {
+        int maxIndex = i, l, r;
+
+        l = leftChild(i);
+
+        if (l < data.length && data[l] < data[maxIndex]) {
+            maxIndex = l;
+        }
+
+        r = rightChild(i);
+
+        if (r < data.length && data[r] < data[maxIndex]) {
+            maxIndex = r;
+        }
+
+        if (i != maxIndex) {
+            swaps.add(new Swap(i, maxIndex));
+
+            int temp = data[maxIndex];
+            data[maxIndex] = data[i];
+            data[i] = temp;
+
+            siftDown(maxIndex);
+        }
+    }
+
+    public void solve() throws IOException {
+        in = new FastScanner();
+        out = new PrintWriter(new BufferedOutputStream(System.out));
+        readData();
+        generateSwaps();
+        writeResponse();
+        out.close();
+    }
+
+    static class Swap {
+        int index1;
+        int index2;
+
+        public Swap(int index1, int index2) {
+            this.index1 = index1;
+            this.index2 = index2;
+        }
+    }
+
+    static class FastScanner {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public FastScanner() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+}
